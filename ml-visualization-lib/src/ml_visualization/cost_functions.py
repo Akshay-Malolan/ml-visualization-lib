6a298ca2077385dae51c76bf838bc17c39d6c211
Akshay-Malolan <135319949+Akshay-Malolan@users.noreply.github.com>
2025-07-20 19:02:38 +0530

    Delete ml-visualization-lib directory

--- ml-visualization-lib/src/ml_visualization/cost_functions.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def compute_cost(x, y, w, b):
-    m = len(y)  # number of training examples
-    cost = (1 / (2 * m)) * np.sum(np.square(np.dot(x, w) + b - y))
-    return cost
